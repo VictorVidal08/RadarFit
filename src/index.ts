e85145302284469ebcf9f770cef1b6b14d0f9d01
VictorVidal08 <victorvidal08@hotmail.com>
2022-11-05 17:09:55 -0300

    fix: port on deploy

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,11 @@ import 'dotenv/config';
 import app from './app';
 import connectToDatabase from './models/connection';
 
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001;
 
 connectToDatabase()
   .then(() => {
-    app.listen(PORT, () => console.log(`Running server on port: ${PORT}`));
+    app.listen(port, () => console.log(`Running server on port: ${port}`));
   })
   .catch((error: any) => {
     console.log('Connection with database generated an error:\r\n');
